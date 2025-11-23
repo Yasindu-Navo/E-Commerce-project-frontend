72a2670955aeabdc68ee996b411ce44f9837e04f
Yasindu Navodh <cst21024@std.uwu.ac.lk>
2025-11-23 14:42:32 +0530

    refactor ProductsGrid and implement ProductList component for better code organization and functionality

--- ecommerece-project/src/pages/home/ProductsGrid.jsx
+++ ecommerece-project/src/pages/home/ProductsGrid.jsx
@@ -1,84 +1,15 @@
-import React, { useState } from 'react'
-import checkmark from "../../assets/images/icons/checkmark.png"
-import { formatMoney } from '../../utils/Money';
-import axios from 'axios';
+
+import ProductList from './ProductList';
 
 function ProductsGrid({ products, cartData }) {
-  const [quantity, setQuantity] = useState(1);
+  
+
   return (
       <div className="products-grid">
           {/* convert each product into Html */}
           {products.map((product) => {
             return (
-              <div key={product.id} className="product-container">
-                <div className="product-image-container">
-                  <img
-                    className="product-image"
-                    alt="product-image"
-                    src={product.image}
-                  />
-                </div>
-
-                <div className="product-name limit-text-to-2-lines">
-                  {product.name}
-                </div>
-
-                <div className="product-rating-container">
-                  <img
-                    className="product-rating-stars"
-                    src={`images/ratings/rating-${
-                      product.rating.stars * 10
-                    }.png`}
-                  />
-                  <div className="product-rating-count link-primary">
-                    {product.rating.count}
-                  </div>
-                </div>
-
-                <div className="product-price">
-                  {formatMoney(product.priceCents)}
-                </div>
-
-                <div className="product-quantity-container">
-                  <select value={quantity}
-                    onChange={(e) => {
-                        e.target.value
-                      }}>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                  </select>
-                </div>
-
-                <div className="product-spacer"></div>
-
-                <div className="added-to-cart">
-                  <img src={checkmark} />
-                  Added
-                </div>
-
-                <button className="add-to-cart-button button-primary"
-                  onClick={async() => {
-                    
-                   await axios.post('api/cart-items', {
-                      productId: product.id,
-                      quantity:1
-                    })
-
-                    await cartData();
-
-                  }}
-                >
-                  Add to Cart
-                </button>
-              </div>
+              <ProductList key={product.id} product={product} cartData={cartData} />
             );
           })}
         </div>
